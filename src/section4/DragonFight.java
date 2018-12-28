aa51818ab2e78c141d32096fd4e4f835fff31d30
Joshua You <JYou12@users.noreply.github.com>
2018-12-28 14:59:26 -0800

    Added the Potion
    I added the potion option so now the player can use a potion that will give them 25 health.

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,22 +12,13 @@ public class DragonFight {
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");
 
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
-		// 2. Create a variable called "playerHealth" to store your health (set it equal
-		// to 100)
 		int playerHealth = 100;
-		// 3. Create a variable called "dragonHealth" to store the dragon's health (set
-		// it equal to 100)
 		int dragonHealth = 500;
-		// 4. Create a variable to hold the damage the player's attack does each round
 		int playerDamage;
-		// 5. Create a variable to hold the damage the dragon's attack does each round
 		int dragonDamage;
 
-		// 6. Delete the slashes at the beginning of the next line.
 		while (playerHealth > 0 && dragonHealth > 0) {
 
-			// 8. Ask the player in a pop-up if they want to attack the dragon with a yell
-			// or a kick
 			boolean pATK = false;
 			int slashUses=10;
 			int uppercutUses=5;
@@ -74,23 +65,7 @@ public class DragonFight {
 				}
 			}
 
-			// 11. Find a random number between 0 and 35 and store it in playerDamage
 
-			// 12. Subtract this number from the player's health
-
-			// 13. If the user's health is less than or equal to 0
-
-			// -- Tell the user that they lost
-
-			// 14. Else if the dragon's health is less than or equal to 0
-
-			// -- Tell the user that the dragon is dead and they took a ton of gold!
-
-			// 15. Else
-
-			// -- Pop up a message that tells the their current health and the dragon's
-			// currently health (Bonus: Also display the amount of health that was lost for
-			// each player this round)
 
 		}
 	}
